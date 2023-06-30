7a33b3f6609f647ad00e1e6ab06a90d131461bcf
DarknessBetrayal <DarknessBetrayal@gmail.com>
2023-07-01 02:07:04 +1200

    Nick Pick Update

--- js/serverstats.js
+++ js/serverstats.js
@@ -1,56 +1,70 @@
 
-// Update the HTML with the countdown button
-const countdownDiv = document.getElementById("countdown");
-countdownDiv.innerHTML = `
+// Update the HTML with the buttons
+const restartDiv = document.getElementById("restart");
+restartDiv.innerHTML = `
   <button class="btn btn-dark" >
-    Restarts Are
+    Restarts Are 5PM NZST (3PM AEST)
   </button>
 `;
-const countdownDiv2 = document.getElementById("countdown2");
-countdownDiv2.innerHTML = `
-  <button class="btn btn-dark">
-    5PM NZST (3PM AEST)
+const discordtDiv = document.getElementById("discord");
+discordtDiv.innerHTML = `
+  <button class="btn btn-primary" >
+    Join our discord for more info
   </button>
 `;
 
-// Make a request to the API...
-fetch("https://arma3-servers.net/api/?object=servers&element=detail&key=Kct2q4OHYjKQcK0So9fxItddzI8kvnafrg")
-  .then(response => {
-    if (!response.ok) {
-      throw new Error("Failed to fetch data. Status: " + response.status);
-    }
-    return response.json(); // Parse the response as JSON
-  })
-  .then(data => {
-    // Extract the desired values
-    const { maxplayers: maxPlayers, players: currentPlayers, is_online: isOnline } = data;
+// Function to fetch and update the server data
+function fetchAndUpdateServerData() {
+  fetch("https://arma3-servers.net/api/?object=servers&element=detail&key=Kct2q4OHYjKQcK0So9fxItddzI8kvnafrg")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Failed to fetch data. Status: " + response.status);
+      }
+      return response.json(); // Parse the response as JSON
+    })
+    .then(data => {
+      // Extract the desired values
+      const { maxplayers: maxPlayers, players: currentPlayers, is_online: isOnline } = data;
 
-    // Update the HTML based on the "is_online" value
-    const dataDiv = document.getElementById("data");
-    if (isOnline === "0") {
-      dataDiv.innerHTML = `
-        <button class="btn btn-secondary btn-lg">Server Offline</button>
-      `;
-    } else {
+      // Update the HTML based on the "is_online" value and player count
+      const dataDiv = document.getElementById("data");
       const ipAddress = "121.99.242.246:2302";
       const steamLink = `steam://rungameid/107410// +connect ${ipAddress}`;
-      dataDiv.innerHTML = `
-        <button class="btn btn-danger btn-lg" onclick="openSteamLink('${steamLink}')" data-bs-toggle="tooltip" data-bs-placement="top" title="Click To Join">
-         Join ${currentPlayers}/${maxPlayers} Players
-        </button>
-      `;
+      if (isOnline === "0") {
+        dataDiv.innerHTML = `
+          <button class="btn btn-secondary btn-lg" data-bs-toggle="tooltip" data-bs-placement="top" title="Check Back Soon">Server Offline</button>
+        `;
+      } else if (currentPlayers === "0") {
+        dataDiv.innerHTML = `
+          <button class="btn btn-success btn-lg" onclick="openSteamLink('${steamLink}')" data-bs-toggle="tooltip" data-bs-placement="top" title="Click To Join">Be The First</button>
+        `;
+      } else {
+        dataDiv.innerHTML = `
+          <button class="btn btn-danger btn-lg" onclick="openSteamLink('${steamLink}')" data-bs-toggle="tooltip" data-bs-placement="top" title="Click To Join">
+           Join ${currentPlayers}/${maxPlayers} Players
+          </button>
+        `;
+      }
       // Initialize Bootstrap tooltip
       const tooltipTriggerEl = document.querySelector("#data [data-bs-toggle='tooltip']");
       const tooltip = new bootstrap.Tooltip(tooltipTriggerEl);
-    }
-  })
-  .catch(error => {
-    console.error("Error:", error);
-    const dataDiv = document.getElementById("data");
-    dataDiv.innerHTML = ``;
-  });
+    })
+    .catch(error => {
+      console.error("Error:", error);
+      const dataDiv = document.getElementById("data");
+      dataDiv.innerHTML = `
+        <button class="btn btn-secondary btn-lg">Server Offline</button>
+      `;
+    });
+}
 
 // Function to open Steam link
 function openSteamLink(link) {
   window.location.href = link;
 }
+
+// Fetch and update the server data immediately
+fetchAndUpdateServerData();
+
+// Schedule periodic updates every 30 seconds
+setInterval(fetchAndUpdateServerData, 30000);
